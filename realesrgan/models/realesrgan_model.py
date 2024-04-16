b7f40a81686e9f0cf8b757712222f7224ac509b3
LinziGosh <1643515720@qq.com>
2024-04-17 01:03:37 +0800

    [ fast test phase ]

--- realesrgan/models/realesrgan_model.py
+++ realesrgan/models/realesrgan_model.py
@@ -3,7 +3,7 @@ import random
 import torch
 from basicsr.data.degradations import random_add_gaussian_noise_pt, random_add_poisson_noise_pt
 from basicsr.data.transforms import paired_random_crop
-from models.srgan_model import SRGANModel_save
+from srgan_model import SRGANModel_save
 from basicsr.utils import DiffJPEG, USMSharp
 from basicsr.utils.img_process_util import filter2D
 from basicsr.utils.registry import MODEL_REGISTRY
